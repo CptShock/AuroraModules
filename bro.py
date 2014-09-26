04f94282f4adea89a07553c4b38158ca24e56a6c
CptShock <CptShock@gmail.com>
2014-09-26 17:37:24 +0200

    Adding module brocode
    This is some very rough code, up for improvement.

---
+++ bro.py
@@ -0,0 +1,30 @@
+import willie
+import willie.module import commands
+import random
+@willie.module.commands('brocode')
+
+def n(bot, trigger):
+	if n == 1:
+		bot.say('You must always have your bro''s back. No exceptions.')
+	elif n == 2:
+		bot.say('When your bro’s girlfriend inquires about his whereabouts you know nothing, always.')
+	elif n == 3:
+		bot.say('You are only obligated to wingman for one bro per social event, after that the bro is on his own.')
+	elif n == 4:
+		bot.say('You must always have your bro''s back. No exceptions.')
+	elif n == 5:
+		bot.say('You must always have your bro''s back. No exceptions.')
+	elif n == 6:
+		bot.say('You must always have your bro''s back. No exceptions.')
+	elif n == 7:
+		bot.say('You must always have your bro''s back. No exceptions.')
+	elif n == 8:
+		bot.say('You must always have your bro''s back. No exceptions.')
+	elif n == 9:
+		bot.say('You must always have your bro''s back. No exceptions.')
+	elif n == 10:
+		bot.say('You must always have your bro''s back. No exceptions.')
+	elif n == 11:
+		bot.say('You must always have your bro''s back. No exceptions.')
+	elif n == 12:
+		bot.say('You must always have your bro''s back. No exceptions.')
