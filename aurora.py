dc7ad019e378ba531adccb3ab945d413b04aa452
CptShock <CptShock@gmail.com>
2014-09-26 18:21:11 +0200

    Quick fix for calling commands

--- aurora.py
+++ aurora.py
@@ -1,6 +1,6 @@
 import willie
 import random
-@willie.module.commands('8')
+@willie.module.commands('aurora')
 def ball(bot, trigger):
     """Ask Aurora your questions and she will answer! Usage: !Aurora <question>"""
     messages = ["It is certain"," It is decidedly so","Without a doubt","Yes definitely","You may rely on it","As I see it yes","Most likely","Outlook good","Yes","Signs point to yes","Reply hazy try again","Ask again later","Better not tell you now","Cannot predict now","Concentrate and ask again","Don't count on it","My reply is no","God says no","Very doubtful","Outlook not so good","Wouldn't hope for it","Not in your wildest dreams","Maybe if Arne learns to grow a beard","Not now, not never","Most Definitely"]
