d21742ac9b936d248eac608f590bec3eec268102
thehuntsman <sander.elsen@pandora.be>
2014-09-30 17:07:38 +0200

    pasta test
    test

--- pastabin.py
+++ pastabin.py
@@ -1,6 +1,15 @@
 from willie.module import commands
-import random
+import urllib2, urllib, optparse, sys, os.path,random
+
+vardevkey = "e556580a88095cbfb11184fa1a97863b"
+vartest = "test123"
+varurl = "http://pastebin.com/api/api_post.php"
+varpaste = "paste"
+varparams = {"api_dev_key":vardevkey,"api_option":varpaste,"api_paste_code":vartest}
 
 @commands('pastabin')
 def ans(bot, trigger):
-	bot.reply("test")
+	fetch(url,params,method)
+def fetch(url, params):
+  varparams = urllib.parse.urlencode(varparams)
+  f = urllib.request.urlopen(varurl, varparams)
