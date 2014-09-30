9472441efbb96bde20ed7ce02fc9495f6bc078cd
thehuntsman <sander.elsen@pandora.be>
2014-09-30 17:20:41 +0200

    SO MANY FIXES!

--- pastabin.py
+++ pastabin.py
@@ -1,14 +1,14 @@
 from willie.module import commands
 import urllib2, urllib, optparse, sys, os.path,random
 
-vardevkey = "e556580a88095cbfb11184fa1a97863b"
-vartest = "test123"
-varurl = "http://pastebin.com/api/api_post.php"
-varpaste = "paste"
-varparams = {"api_dev_key":vardevkey,"api_option":varpaste,"api_paste_code":vartest}
 
 @commands('pastabin')
 def ans(bot, trigger):
+	vardevkey = "e556580a88095cbfb11184fa1a97863b"
+	vartest = "test123"
+	varurl = "http://pastebin.com/api/api_post.php"
+	varpaste = "paste"
+	varparams = {"api_dev_key":vardevkey,"api_option":varpaste,"api_paste_code":vartest}
 	fetch(varurl,varparams)
 def fetch(url, params):
   params = urllib.urlencode(varparams)
