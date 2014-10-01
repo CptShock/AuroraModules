4b3e28bdce5b79e747640b7086de29bdd195cffa
thehuntsman <sander.elsen@pandora.be>
2014-10-01 13:25:55 +0200

    fixed pasta test error

--- nieuws.py
+++ nieuws.py
@@ -2,4 +2,8 @@
 # -*- coding: latin-1 -*-
 from __future__ import unicode_literals;from willie import web;from willie.module import commands;import xml.etree.ElementTree as ET
 @commands('nieuws')
-def nieuws(bot, trigger): bot.say(ET.fromstring(web.get('http://www.demorgen.be/nieuws/rss.xml')).find('channel/item/title').text)
+inputtext = ""
+output = ""
+def nieuws(bot, trigger): 
+	inputtext = ET.fromstring(web.get('http://www.demorgen.be/nieuws/rss.xml')).find('channel/item/title').text.decod('ascii')
+	bot.say(inputtext)
