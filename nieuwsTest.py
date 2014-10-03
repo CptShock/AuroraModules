d500a5c6975e16c1f3a199d17161d646715dc96a
thehuntsman <sander.elsen@pandora.be>
2014-10-03 16:58:30 +0200

    test

--- nieuwsTest.py
+++ nieuwsTest.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python
-# -*- coding: latin-1 -*-
 import feedparser
 @commands('nieuwsT')
 def nieuws(bot, trigger):
-	python_wiki_rss_url = "http://www.python.org/cgi-bin/moinmoin/" \
-                      "RecentChanges?action=rss_rc"
-    feed = feedparser.parse( python_wiki_rss_url )
-    bot.say(feed)
+	url = "http://www.demorgen.be/nieuws/rss.xml"
+	feed = feedparser.parse(nurl )
+	bot.say(feed)
+	bot.say("end")
+    
     
