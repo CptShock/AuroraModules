e1b5af91686ee3c0e878932afc756103bb351f84
thehuntsman <sander.elsen@pandora.be>
2014-10-03 09:11:44 +0200

    nieuwsT

--- nieuwsTest.py
+++ nieuwsTest.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: latin-1 -*-
-from __future__ import unicode_literals;from willie import web;from willie.module import commands;import xml.etree.ElementTree as ET
+from __future__ import unicode_literals;from willie import web;from willie.module import commands;import xml.etree.ElementTree as ET;import codecs
 @commands('nieuwsT')
 def nieuws(bot, trigger):
-	content = unicode((ET.fromstring(web.get('http://www.demorgen.be/nieuws/rss.xml')).find('channel/item/title').text).content.strip(codecs.BOM_UTF8), 'utf-8')
-	parser.parse(StringIO.StringIO(content))
-	bot.say(content)
+	bot.say(ET.fromstring(web.get('http://www.demorgen.be/nieuws/rss.xml')).find('channel/item/title').text..encode('ascii', 'ignore'))
