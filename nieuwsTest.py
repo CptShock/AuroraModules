5a8d0dab50cef0f9d29a8cd4642c30ef07599cc1
thehuntsman <sander.elsen@pandora.be>
2014-10-03 20:35:08 +0200

    test

--- nieuwsTest.py
+++ nieuwsTest.py
@@ -1,9 +1,8 @@
-import feedparser
+import feedparser,time,willie
 @commands('nieuwsT')
 def nieuws(bot, trigger):
 	url = "http://www.demorgen.be/nieuws/rss.xml"
-	feed = feedparser.parse(nurl )
+	feed = feedparser.parse( url )
 	bot.say(feed)
 	bot.say("end")
-    
-    
+	time.sleep(20)
