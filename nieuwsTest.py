2b3b0855a3b44b307483bd096572b8bd70d8a0a0
thehuntsman <sander.elsen@pandora.be>
2014-10-03 20:54:57 +0200

    test

--- nieuwsTest.py
+++ nieuwsTest.py
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 import feedparser,time,willie
-=======
-import willie
-import feedparser
->>>>>>> 83c99e723d85c8a52066a9da9033c7f200bb9edc
-@commands('nieuwsT')
+@willie.module.commands('nieuwsT')
 def nieuws(bot, trigger):
-	url = "http://www.demorgen.be/nieuws/rss.xml"
+        url = "http://www.demorgen.be/nieuws/rss.xml"
 	feed = feedparser.parse( url )
 	bot.say(feed)
 	bot.say("end")
