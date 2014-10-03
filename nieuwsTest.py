52c149eb6529b3bf7c9c1535f5631595b0810e89
thehuntsman <sander.elsen@pandora.be>
2014-10-03 09:55:07 +0200

    test

--- nieuwsTest.py
+++ nieuwsTest.py
@@ -3,5 +3,4 @@
 from __future__ import unicode_literals;from willie import web;from willie.module import commands;import xml.etree.ElementTree as ET;import codecs
 @commands('nieuwsT')
 def nieuws(bot, trigger):
-	text = ET.fromstring(web.get('http://www.demorgen.be/nieuws/rss.xml')).find('channel/item/title').text.encode('ascii', 'ignore')
-	bot.say(text+"123")
+	text = ET.fromstring(web.get('http://www.demorgen.be/nieuws/rss.xml').encode('ascii', 'ignore')).find('channel/item/title').text
