7d6c55e3cee63e89b5a1f1d779558fcd738521d2
thehuntsman <sander.elsen@pandora.be>
2014-10-09 11:42:06 +0200

    test

--- nieuwstest2.py
+++ nieuwstest2.py
@@ -6,4 +6,4 @@ Licensed under the GNU Lesser General Public License Version 3 (or greater at yo
 """
 from __future__ import unicode_literals;from willie import web;from willie.module import commands;import xml.etree.ElementTree as ET
 @commands('nieuwsT2')
-def fmylife(bot, trigger): bot.say(ET.fromstring(web.get('http://www.demorgen.be/nieuws/rss.xml')).find('channel/text').text)
+def fmylife(bot, trigger): bot.say(ET.fromstring(web.get('http://www.demorgen.be/nieuws/rss.xml')).find('channel/title').text)
