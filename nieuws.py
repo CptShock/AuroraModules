cf8b5bfe6ed0180b0caf017074b2c76706130469
thehuntsman <sander.elsen@pandora.be>
2014-10-14 15:24:45 +0200

    nieuwsT>>nieuws

--- nieuws.py
+++ nieuws.py
@@ -1,11 +1,1 @@
-import feedparser,time,willie
-@willie.module.commands('nieuwsT')
-def nieuws(bot,trigger):
-    url = 'http://www.demorgen.be/nieuws/rss.xml'
-    feed = feedparser.parse(url)
-    bot.say(str(feed.feed.title))
-    try:
-        bot.say(str(feed.entries[int(trigger.group(2))].title))
-        bot.say(str(feed.entries[int(trigger.group(2))].link))
-    except:
-        bot.say("try another number")
+import feedparser,time,willie@willie.module.commands('nieuws')def nieuws(bot,trigger):    url = 'http://www.demorgen.be/nieuws/rss.xml'    feed = feedparser.parse(url)    bot.say(str(feed.feed.title))    try:        bot.say(str(feed.entries[int(trigger.group(2))].title))        bot.say(str(feed.entries[int(trigger.group(2))].link))    except:        bot.say("try another number")
