5ae16f3e59021812957142918057ee586971e820
thehuntsman <sander.elsen@pandora.be>
2014-10-15 10:04:41 +0200

    CAKE

---
+++ cake.py
@@ -0,0 +1,15 @@
+from willie.module import commands
+import random
+
+@commands('cake')
+def ans(bot, trigger):
+	bot.say("             _.-.")
+    bot.say("           .' _  \")
+    bot.say("         ,'  (_)  \ _")
+    bot.say("     _.-|`-._      \_'--._")
+	bot.say("   .' .-(=._ `-._   \ '-. `.")
+	bot.say("  /  /  |   `=.  `-._\   \  \")
+	bot.say(" |  |    `-._  `=._  | .  |  |")
+	bot.say("  \  \  ;' .,`--._ `=| ' /  /")
+ 	bot.say("   `._``--..._____`--'-''_.'")
+	bot.say("      `--.._________..--'")
